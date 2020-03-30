570cf68bc185c9a57f0770d585e3e5e296074ecc
adit0503 <adit0503@gmail.com>
2020-03-30 22:43:15 +0530

    chapter 1

--- start.py
+++ start.py
@@ -1,2 +1,42 @@
-import pandas
 import nltk
+from nltk.book import *
+text1
+type(text1)
+s = str(text1)
+s
+
+text1.concordance('monstrous')
+text1.similar('monstrous')
+text1.common_contexts(['monstrous','curious']) #<text obj>.common_contexts()
+
+len(text1)
+len(set(text1))
+
+word_list = set(text1)
+word_dict = {}
+for wl in word_list:
+    word_dict[wl] = text1.count(wl)
+list(word_dict.keys())
+
+fd = FreqDist(text1)
+len(fd.keys())
+fd.keys()[:10]
+
+count_lol = text5.count('lol')
+len_text5 = len(text5)
+percent_lol = (count_lol / len_text5)*100
+percent_lol
+
+def word_percentage(word,text)-> float:
+    count_word = text.count(word)
+    len_text = len(text)
+    return round((count_word/len_text)*100 , 2)
+word_percentage('lol',text5)
+
+sent1
+sent2
+
+s = sent1 + sent2
+s
+s.count('.')
+len(s)
