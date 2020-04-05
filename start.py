d27cfdc85bb9420bcdf5bf92d3c13ab3656e2fc8
adit0503 <adit0503@gmail.com>
2020-04-05 15:14:13 +0530

    tokenizer used

--- start.py
+++ start.py
@@ -1,5 +1,9 @@
 import nltk
 from nltk.book import *
+
+example_sent = " ".join(sent1+sent2+sent3+sent4)
+print(example_sent)
+
 text1
 type(text1)
 s = str(text1)
@@ -19,8 +23,15 @@ for wl in word_list:
 list(word_dict.keys())
 
 fd = FreqDist(text1)
+fd
+fd.plot(50, cumulative=True)
 len(fd.keys())
-fd.keys()[:10]
+vocab = []
+for k in fd.keys():
+    vocab.append(k)
+vocab[:10]
+fd['whale']
+text1.count('whale')
 
 count_lol = text5.count('lol')
 len_text5 = len(text5)
@@ -33,10 +44,6 @@ def word_percentage(word,text)-> float:
     return round((count_word/len_text)*100 , 2)
 word_percentage('lol',text5)
 
-sent1
-sent2
-
-s = sent1 + sent2
-s
-s.count('.')
-len(s)
+v = set(text1)
+long_words = [w for w in v if len(w)>10]
+sorted(long_words)
