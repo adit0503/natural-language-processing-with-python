150511d020bcf569c694bc534371ab9cd2d1d4e2
adit0503 <adit0503@gmail.com>
2020-04-05 18:50:07 +0530

    stopwords used

--- sentdex_video2.py
+++ sentdex_video2.py
@@ -1,19 +1,18 @@
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-#tokenizing, corpora, lexicon
+# text-preprocessing : stopwords - useless words that dont provide any insight to what you are doing. contextual based. remove fluffy words !
 
 example_sentence = "NLP is important for scientific, economic, social, and cultural reasons. NLP is experiencing rapid growth as its theories and methods are deployed in a variety of new language technologies. For this reason it is important for a wide range of people to have a working knowledge of NLP. Within industry, this includes people in human-computer interaction, business information analysis, and web software development. Within academia, it includes people in areas from humanities computing and corpus linguistics through to computer science and artificial intelligence. (To many people in academia, NLP is known by the name of 'Computational Linguistics.')"
+tokenized = word_tokenize(example_sentence)
 
-tokenized_sentence = sent_tokenize(example_sentence)
-print(type(tokenized_sentence[0]))
-print(type(tokenized_sentence))
+stop_words = set(stopwords.words('english'))
 
-for s in tokenized_sentence:
-    print(s)
+filtered_sentence = []
 
-tokenized_word = word_tokenize(example_sentence)
-print(tokenized_word)
-for w in tokenized_word:
-    if w.isalpha():
-        print(w)
+for t in tokenized:
+    if t not in stop_words:
+        filtered_sentence.append(t)
+print(filtered_sentence)
+print(len(tokenized)-len(filtered_sentence))
